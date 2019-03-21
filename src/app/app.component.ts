62dfe5e92a3ba38650f99659ff5df607cef1ff20
Nasir Mohammad <nasir.m23@hotmail.com>
2019-03-21 18:40:54 -0400

    init angular app

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'hfmh-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'hands-for-mental-health-app';
+}
